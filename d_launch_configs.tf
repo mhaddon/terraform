482df7c852b15b3ece1e4c8e561e033473f69539
Michael Haddon <dis_connected@ymail.com>
2017-09-22 15:27:46 +0200

    Changed image to coreos

--- d_launch_configs.tf
+++ d_launch_configs.tf
@@ -1,6 +1,6 @@
 resource "aws_launch_configuration" "as_conf" {
   name          = "web_config"
-  image_id      = "ami-efe09bf8"
+  image_id      = "ami-e2d33d98"
   instance_type = "t2.micro"
 
   lifecycle {
