ead166e2e869298e11c9ae88c9f49607f6d6ffa1
Michael Haddon <dis_connected@ymail.com>
2017-09-26 10:52:51 +0200

    removed route

--- stages/one/f_routes.tf
+++ stages/one/f_routes.tf
@@ -1,7 +1,3 @@
-resource "aws_route53_zone" "primary" {
-  name = "devops-training.lunatech.com"
-}
-
 resource "aws_route53_record" "www" {
   zone_id = ""
   name    = "db.devops-training.lunatech.com"
