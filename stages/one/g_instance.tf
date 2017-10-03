e6a8d28da1b2cb6317ab0fd7f3ee99c380670e5b
Michael Haddon <dis_connected@ymail.com>
2017-10-03 17:30:39 +0200

    added cloudinit

--- stages/one/g_instance.tf
+++ stages/one/g_instance.tf
@@ -5,6 +5,23 @@ resource "aws_instance" "maintenance" {
   vpc_security_group_ids = ["${aws_security_group.allow_all.id}"]
   key_name = "aws_test"
   associate_public_ip_address = true
+  user_data          = "${file("init.conf")}"
+
+  connection {
+    user = "ubuntu"
+    private_key = "${file("aws_test2.pem")}"
+    agent = false
+  }
+
+  provisioner "file" {
+    source      = "aws_test2.pem"
+    destination = "/tmp/aws_test.pem"
+  }
+
+  provisioner "file" {
+    source      = "../../../mysql/airportdemo.sql"
+    destination = "aws_test2.pem"
+  }
 
   tags {
     name="maintenance"
